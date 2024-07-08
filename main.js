a03289e15ddacdf6cfb49054b8aaba59fed94482
tự học để làm JS <bdphuong1123@gmail.com>
2024-07-08 18:41:08 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//hàm hiển thị hộp thoại lên trên cho người dùng đọc
+alert("đang buồn ngủ quá ");
+alert("cố gắng thức học js ");
+alert("đói bụng ");
+/*
+ví dụ
+ví dụ
+*/
+// hàm hiển thị hộp nhập liệu
+prompt("mời nhập tên ");
+//khai báo biến
+var hoTen;
+let doing = 10;
+// const soLan;
+
+// khởi tạo biến
+var hoTen = "bùi duy phương ";
+let dangLAm = "học JS";
+const soLuong = 10;
+//xuất giá trị biến
+console.log(hoTen + soLuong);
+
+console.log(hoTen + "tháng");
+// thử thay đổi giá trị biến
+hoTen = "Phương";
+doing = 112;
+// thay đổi biến hoTen để xuất người dùng nhập hộp liệu lên console
+hoTen = prompt("mời nhập tên "); // vì đã gọi var ở trên đầu rồi nên ko cần gọi var ở đây nữa
+console.log(hoTen);
